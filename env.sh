e815faa57944b8f9d863adb07ce7b8bdb8a26900
vicotorlu <sean.lu91@gmail.com>
2022-12-04 12:58:26 +0000

    update script

--- env.sh
+++ env.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+ip=`ip addr | grep -v inet6 | grep inet | grep -v docker | grep -v -w lo | grep -v "br-" | grep -Eo "172.[0-9.]+*" | head -n 1`
 export MAX_NODE_IDX=15
+export HOSTIP=$ip
